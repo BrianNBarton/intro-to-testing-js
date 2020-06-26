18e4efb57aff44ea2e016872fffacda1420194df
Brian Barton <brian.n.barton@gmail.com>
2020-06-26 10:59:53 -0500

    completed unit testing exercise

--- code.js
+++ code.js
@@ -4,24 +4,39 @@ function helloWorld() {
 }
 
 
-        function sayHello(input) {
-    if (typeof input === "undefined"){
+function sayHello(input) {
+    if (typeof input === "undefined") {
         return "Hello, World!";
-    } else if (typeof input === "boolean"){
+    } else if (typeof input === "boolean") {
         return "Hello, World!";
-    } else if (input === null){
+    } else if (input === null) {
         return "Hello, World!";
     } else if (input === "") {
         return "Hello, World!";
-    } else if (input === 2.3){
+    } else if (input === 2.3) {
         return "Hello, World!";
     } else {
         return "Hello, " + input + "!";
     }
 }
-        function isFive(input) {
-   return input == 5;
+
+function isFive(input) {
+    return input == 5;
 }
 
 
-    
+function isEven(n) {
+    return n % 2 === 0;
+}
+
+function isVowel(str) {
+    const noCase = typeof str === "string" ? str.toLowerCase() : "";
+    return noCase === 'a' || noCase === 'e' || noCase === 'i' || noCase === 'o' || noCase === 'u';
+}
+
+function add(x, y) {
+    // if (typeof isNaN(x) && isNaN(y)) {
+    //     return NaN;
+    // } else {
+    return parseInt(x) + parseInt(y);
+}
--- tests.js
+++ tests.js
@@ -1,25 +1,23 @@
-
-
 // Unit tests for the helloWorld function
 // exercise 1
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
         expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
+    it('should return a string when called', function () {
         expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
+    it('should return the string "Hello, World!" when executed', function () {
         expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
+    it("should never return 'undefined' when called", function () {
         expect(helloWorld()).not.toBe(undefined);
     });
 });
 
 
 //exercise 2
-describe('sayHello', function() {
+describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
@@ -64,7 +62,7 @@ describe('sayHello', function() {
 });
 
 
-describe('isFive', function() {
+describe('isFive', function () {
     it('should be a defined function', function () {
         expect(typeof isFive).toBe('function');
     });
@@ -76,31 +74,92 @@ describe('isFive', function() {
     });
 });
 
-//
-//
-// // exercise 10
-// describe('isFive', function() {
-//     it('should be a defined function', function () {
-//         expect(typeof isFive).toBe('function');
-//     });
-//     it('should return a boolean when called', function () {
-//         expect(typeof isFive()).toBe("boolean");
-//     });
-//
-// });
+
+describe('isEven', function () {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it('(2)should return true', function () {
+        expect(isEven(2)).toBe(true);
+    });
+    it('(2)should return true', function () {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('(3)should return false', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('(banana)should return false', function () {
+        expect(isEven("banana")).toBe(false);
+    });
+    it('(8)should return false', function () {
+        expect(isEven("8")).toBe(true);
+    });
+    it('(Infinity)should return false', function () {
+        expect(isEven("Infinity")).toBe(false);
+    });
+    it('(true)should return false', function () {
+        expect(isEven(true)).toBe(false);
+    });
+    it('()should return false', function () {
+        expect(isEven()).toBe(false);
+    });
+});
 
 
+describe('isVowel', function () {
+    it('should be a defined function', function () {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('(true)should return false', function () {
+        expect(isVowel(true)).toBe(false);
+    });
+    it('(true)should return false ', function () {
+        expect(isVowel(false)).toBe(false);
+    });
+    it('("a")should return true', function () {
+        expect(isVowel("a")).toBe(true);
+    });
+    it('("A")should return true', function () {
+        expect(isVowel("A")).toBe(true);
+    });
+    it('("y")should return false', function () {
+        expect(isVowel("y")).toBe(false);
+    });
+    it('(4)should return false', function () {
+        expect(isVowel(4)).toBe(false);
+    });
+});
 
 
+describe('add', function () {
+    it('should be a defined function', function () {
+        expect(typeof add).toBe('function');
+    });
+    it('(2, 3)should return 5', function () {
+        expect(add(2, 3)).toBe(5);
+    });
+    it('(-3, -9)should return -12', function () {
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it('("5", 6)should return 5', function () {
+        expect(add("5", 6)).toBe(11);
+    });
+    it('("-4", "10")should return 5', function () {
+        expect(add("-4", "10")).toBe(6);
+    });
+    it('("banana", "split")should return 5', function () {
+        expect(add("banana", "split")).toBeNaN();
+    });
 
-
-// describe('isFive', function() {
-//     it('should be a defined function', function () {
-//         expect(typeof isFive).toBe('function');
-//         it('should return a boolean when called', function () {
-//             expect(typeof isFive()).toBe("boolean");
-//         });
-//     });
-
+    it('("banana", "split")should return NaN', function () {
+        expect(add("banana", "split")).toBeNaN();
+    });
+    it('(2, "apples")should return NaN', function () {
+        expect(add(2, "apples")).toBeNaN();
+    });
+    it('()should return NaN', function () {
+        expect(add()).toBeNaN();
+    });
+});
 
 
